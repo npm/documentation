5793b3b8cc7cd10f4b550eafbee2901c0bb190e8
Edward Thomson <ethomson@edwardthomson.com>
2021-02-22 13:37:12 +0000

    Update to gatsby-theme-doctornpm 1.4.0

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,6 @@ module.exports = {
     title: 'npm Docs',
     shortName: 'npm',
     description: 'Documentation for the npm registry, website, and command-line interface',
-    editOnGitHub: false,
-    showContributors: false,
-    repo: {
-      url: 'https://github.com/npm/documentation',
-      defaultBranch: 'main',
-    },
     imageUrl: 'https://user-images.githubusercontent.com/29712634/81721690-e2fb5d80-9445-11ea-8602-4b2294c964f3.png',
   },
   plugins: [
@@ -16,6 +10,13 @@ module.exports = {
       resolve: 'gatsby-theme-doctornpm',
       options: {
         icon: './src/images/npm-favicon.png',
+        editOnGitHub: false,
+        showContributors: false,
+        repo: {
+          url: 'https://github.com/npm/documentation',
+          defaultBranch: 'main',
+        },
+        pathPrefix: process.env.PATH_PREFIX || ''
       },
     },
     'gatsby-plugin-meta-redirect'
