af74c350ae1a92f5abaf3982c5ec5a20d7d42898
Kirill Pavlov <pavlov-kirill@github.com>
2022-10-12 13:48:13 +0400

    added keyboard navigation

--- theme/src/components/container.js
+++ theme/src/components/container.js
@@ -1,9 +1,17 @@
 import {Box} from '@primer/components'
 import React from 'react'
+import { activateArrowKeyNavigation } from '../utils/accessibility'
 
 function Container({children}) {
+  const containerRef = React.createRef()
+
+  React.useEffect(() => {
+    const arrowKeyNavigation = activateArrowKeyNavigation(containerRef.current, 'li > a')
+    return arrowKeyNavigation.deactivate
+  }, [containerRef.current])
+
   return (
-    <Box id="skip-nav" width="100%" maxWidth={960} p={5} mx="auto">
+    <Box id="skip-nav" width="100%" maxWidth={960} p={5} mx="auto" ref={containerRef}>
       {children}
     </Box>
   )
