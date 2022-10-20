2d34d1752d0b9677a70b575389d2fccdaafc7ec2
Shivangi Mittal <shmittal@ip-10-165-0-56.us-west-2.compute.internal>
2022-10-20 11:47:22 +0530

    Adding arrow key navigation

--- theme/src/components/table-of-contents.js
+++ theme/src/components/table-of-contents.js
@@ -1,9 +1,17 @@
 import {Box, Link} from '@primer/components'
 import React from 'react'
+import { activateArrowKeyNavigation } from '../utils/accessibility'
 
 function TableOfContents({items, depth}) {
+  const tableOfContentsRef = React.createRef()
+
+  React.useEffect(() => {
+    const arrowKeyNavigation = activateArrowKeyNavigation(tableOfContentsRef.current, 'li > a')
+    return arrowKeyNavigation.deactivate
+  }, [tableOfContentsRef.current])
+
   return (
-    <Box key={items} as="ul" m={0} p={0} css={{listStyle: 'none', lineHeight: '1.4em'}}>
+    <Box key={items} as="ul" m={0} p={0} css={{listStyle: 'none', lineHeight: '1.4em'}} ref={tableOfContentsRef}>
       {items.map(item => (
         <Box as="li" key={item.url} pl={depth > 0 ? 3 : 0}>
           <Link key={item.title} display="inline-block" py={1} href={item.url} color="gray.6">
