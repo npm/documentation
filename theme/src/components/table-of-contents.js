910f3e6d8eea8a8070ae95210c023990d2d3adb8
Kirill Pavlov <pavlov-kirill@github.com>
2022-11-02 10:36:09 +0400

    added roles

--- theme/src/components/table-of-contents.js
+++ theme/src/components/table-of-contents.js
@@ -3,9 +3,9 @@ import React from 'react'
 
 function TableOfContents({items, depth, labelId}) {
   return (
-    <Box key={items} as="ul" m={0} p={0} css={{listStyle: 'none', lineHeight: '1.4em'}} aria-labelledby={labelId}>
+    <Box key={items} as="ul" role='list' m={0} p={0} css={{listStyle: 'none', lineHeight: '1.4em'}} aria-labelledby={labelId}>
       {items.map(item => (
-        <Box as="li" key={item.url} pl={depth > 0 ? 3 : 0}>
+        <Box as="li" role='listitem' key={item.url} pl={depth > 0 ? 3 : 0}>
           <Link key={item.title} display="inline-block" py={1} href={item.url} color="gray.6">
             {item.title}
           </Link>
