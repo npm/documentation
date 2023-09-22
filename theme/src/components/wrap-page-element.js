47de99056bcd9257da02bef73c85e751b1002c26
Luke Karrys <luke@lukekarrys.com>
2023-09-22 13:39:45 -0700

    fix: set input placeholder text color to higher contrast

--- theme/src/components/wrap-page-element.js
+++ theme/src/components/wrap-page-element.js
@@ -1,10 +1,18 @@
 import {BaseStyles} from '@primer/components'
 import React from 'react'
 import SkipLink from './skip-link'
+import {createGlobalStyle} from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  ::placeholder {
+    color: #dddddd;
+  }
+`
 
 function wrapPageElement({element}) {
   return (
     <BaseStyles>
+      <GlobalStyle />
       <SkipLink />
       {element}
     </BaseStyles>
