afacd2b1c3e5a37cc421ca7ea6ada0012da2e4e7
Luke Karrys <luke@lukekarrys.com>
2023-09-25 18:03:38 -0700

    fix: cleanup hooks to use useBreakpoint for screen width changes

--- theme/src/use-breakpoint.js
+++ theme/src/use-breakpoint.js
@@ -1,29 +1,35 @@
 import React from 'react'
+import {ThemeContext} from 'styled-components'
 
-function useBreakpoint(breakpoint, minMax = 'min') {
-  // Handle string values from themes with units at the end
-  if (typeof breakpoint === 'string') {
-    breakpoint = parseInt(breakpoint, 10)
-  }
+const getMatches = query => (typeof window !== 'undefined' ? window.matchMedia(query).matches : false)
 
-  const matchMedia = React.useMemo(() => {
-    if (typeof window === 'undefined') {
-      const eventTarget = new EventTarget()
-      eventTarget.matches = false
-      return eventTarget
-    }
-    return window.matchMedia(`(${minMax}-width: ${breakpoint - (minMax === 'min' ? 0 : 1)}px)`)
-  }, [breakpoint, minMax])
-
-  const [matches, setMatches] = React.useState(matchMedia.matches)
-  const handleChange = React.useCallback(() => setMatches(matchMedia.matches), [matchMedia])
+// The MIT License (MIT)
+// Copyright (c) 2020 Julien CARON
+// https://github.com/juliencrn/usehooks-ts/blob/master/packages/usehooks-ts/src/useMediaQuery/useMediaQuery.ts
+export function useMediaQuery(query) {
+  const [matches, setMatches] = React.useState(getMatches(query))
+  const handleChange = React.useCallback(() => setMatches(getMatches(query)), [query])
 
   React.useEffect(() => {
+    handleChange()
+    const matchMedia = window.matchMedia(query)
     matchMedia.addEventListener('change', handleChange)
     return () => matchMedia.removeEventListener('change', handleChange)
-  }, [matchMedia, handleChange])
+  }, [query, handleChange])
 
   return matches
+}
+
+export function useBreakpoint(breakpoint, minMax = 'min') {
+  // Handle string values from themes with units at the end
+  const px = typeof breakpoint === 'string' ? parseInt(breakpoint, 10) : breakpoint
+  return useMediaQuery(`(${minMax}-width: ${px - (minMax === 'min' ? 0 : 1)}px)`)
+}
+
+// a common breakpoint where things change on mobile
+export function useIsMobile() {
+  const theme = React.useContext(ThemeContext)
+  return useBreakpoint(theme.breakpoints[2], 'max')
 }
 
 export default useBreakpoint
