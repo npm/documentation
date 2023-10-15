d8c4cf7014d4ce2c65d761600ea803748f468005
Luke Karrys <luke@lukekarrys.com>
2023-10-15 10:43:57 -0700

    chore: lint from root for frontend deps

--- theme/scripts/template-oss/index.js
+++ theme/scripts/template-oss/index.js
@@ -1,18 +1,4 @@
 module.exports = {
   ...require('../../../scripts/template-oss'),
-  workspaceRepo: {
-    add: {
-      '.github/settings.yml': false,
-    },
-  },
-  workspaceModule: {
-    add: {
-      'package.json': {file: 'pkg.json', overwrite: false},
-      '.eslintrc.js': false,
-    },
-  },
   allowPaths: ['/src', '/gatsby-*.js', '/jest*.js', '/index.js', '/.prettierIgnore'],
-  requiredPackages: {
-    devDependencies: [],
-  },
 }
