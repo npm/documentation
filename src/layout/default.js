608753fc5d6dc7b73c7747a99da8e602eaf892c9
Luke Karrys <luke@lukekarrys.com>
2023-10-17 09:27:49 -0700

    Update H1 styles

--- src/layout/default.js
+++ src/layout/default.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import {Box, Heading} from '@primer/react'
+import {Box} from '@primer/react'
 import PageFooter from '../components/page-footer'
 import * as TableOfContents from '../components/table-of-contents'
 import VariantSelect from '../components/variant-select'
 import Breadcrumbs from '../components/breadcrumbs'
 import withLayout from '../layout'
 import {SKIP_NAV} from '../constants'
+import {H1} from '../mdx'
 
 const Layout = ({children, pageContext: {frontmatter}}) => {
   const {title, description} = frontmatter
@@ -26,9 +27,7 @@ const Layout = ({children, pageContext: {frontmatter}}) => {
       <Box sx={{width: '100%', maxWidth: '960px'}}>
         <Box sx={{mb: 4}} {...SKIP_NAV}>
           <Breadcrumbs />
-          <Heading as="h1" sx={{fontSize: 7}}>
-            {title}
-          </Heading>
+          <H1>{title}</H1>
           {description ? <Box sx={{fontSize: 3, mb: 3}}>{description}</Box> : null}
         </Box>
         <VariantSelect />
--- src/layout/hero.js
+++ src/layout/hero.js
@@ -1,15 +1,30 @@
 import React from 'react'
-import {Box} from '@primer/react'
+import {Box, Heading, Text, ThemeProvider} from '@primer/react'
 import Container from '../components/container'
-import Hero from '../components/hero'
 import withLayout from '../layout'
 import {SKIP_NAV} from '../constants'
+import useSiteMetadata from '../hooks/use-site-metadata'
 
-const HeroLayout = ({children}) => (
-  <Box sx={{width: '100%'}} {...SKIP_NAV}>
-    <Hero />
-    <Container>{children}</Container>
-  </Box>
-)
+const HeroLayout = ({children}) => {
+  const {title, description} = useSiteMetadata()
+
+  return (
+    <Box sx={{width: '100%'}} {...SKIP_NAV}>
+      <ThemeProvider colorMode="night" nightScheme="dark_dimmed">
+        <Box sx={{bg: 'canvas.inset', py: 6}}>
+          <Container>
+            <Heading as="h1" sx={{color: 'fg.default', fontSize: 7, m: 0}}>
+              {title}
+            </Heading>
+            <Text as="p" sx={{m: 0, color: 'fg.onEmphasis', fontSize: 4}}>
+              {description}
+            </Text>
+          </Container>
+        </Box>
+      </ThemeProvider>
+      <Container>{children}</Container>
+    </Box>
+  )
+}
 
 export default withLayout(HeroLayout)
