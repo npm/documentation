bdc7928f0954f28abb9af0f557fb951ae5568cd3
Luke Karrys <luke@lukekarrys.com>
2023-10-17 14:02:38 -0700

    Use CSS only method to scroll pre

--- src/mdx/code.js
+++ src/mdx/code.js
@@ -1,52 +1,98 @@
 import React from 'react'
-import {Box, Text} from '@primer/react'
+import {Box, Text, Button, Octicon} from '@primer/react'
 import Highlight, {defaultProps} from 'prism-react-renderer'
 import githubTheme from 'prism-react-renderer/themes/github'
-import ClipboardCopy from '../components/clipboard-copy'
-import useScrollSize from '../hooks/use-scroll-size'
+import {CheckIcon, CopyIcon} from '@primer/octicons-react'
+import copy from 'copy-to-clipboard'
+import {announce} from '../util/aria-live'
 
-function Code({className: language = '', children}) {
-  const code = children.trim()
-  const {scrollRef, paddingRef, size} = useScrollSize()
+function ClipboardCopy({value, ...props}) {
+  const [copied, setCopied] = React.useState(false)
+
+  React.useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (copied) {
+        setCopied(false)
+      }
+    }, 1000)
+
+    return () => clearTimeout(timeout)
+  }, [copied])
 
   return (
-    <Box sx={{position: 'relative'}}>
-      <div ref={paddingRef}>
-        <Box sx={{position: 'absolute', top: 0, right: 0, p: 2, zIndex: 1}}>
-          <ClipboardCopy value={code} />
-        </Box>
-      </div>
-      <Highlight {...defaultProps} code={code} language={language.replace(/language-/, '')} theme={githubTheme}>
-        {({className, style, tokens, getLineProps, getTokenProps}) => (
+    <Button
+      {...props}
+      aria-label="Copy to clipboard"
+      onClick={() => {
+        copy(value)
+        setCopied(true)
+        announce(`Copied to clipboard`)
+      }}
+    >
+      <Octicon icon={copied ? CheckIcon : CopyIcon} sx={{color: copied ? 'success.fg' : 'fg.muted'}} />
+    </Button>
+  )
+}
+
+// export default ClipboardCopy
+
+function Code({className, children}) {
+  const language = className ? className.replace(/language-/, '') : ''
+  const code = children.trim()
+
+  return (
+    <Highlight {...defaultProps} code={code} language={language} theme={githubTheme}>
+      {({className, style, tokens, getLineProps, getTokenProps}) => (
+        <Box
+          sx={{
+            // Make <pre> adjust to the width of the container
+            // https://stackoverflow.com/a/14406386
+            display: 'table',
+            tableLayout: 'fixed',
+            width: '100%',
+            mb: 3,
+          }}
+        >
           <Box
-            tabIndex={0}
-            as="pre"
-            className={className}
             style={style}
             sx={{
-              overflow: 'auto',
+              display: 'flex',
+              justifyContent: 'space-between',
+              flexDirection: 'row-reverse',
               borderRadius: 2,
-              mt: 0,
-              mb: 3,
-              p: 3,
               borderStyle: 'solid',
               borderWidth: 1,
               borderColor: 'border.muted',
             }}
           >
-            {/* This is the scroll handle, it is supposed to be focused with keyboard and scroll a wide codebox horizontally */}
-            <div aria-hidden="true" style={{visibility: 'hidden', position: 'absolute', ...size}} ref={scrollRef} />
-            {tokens.map((line, i) => (
-              <div key={i} {...getLineProps({line, key: i})}>
-                {line.map((token, key) => (
-                  <Text key={key} {...getTokenProps({token, key})} sx={{fontFamily: 'mono', fontSize: 1}} />
+            <ClipboardCopy
+              value={code}
+              sx={{
+                borderRadius: 0,
+                borderStyle: 'solid',
+                borderWidth: 1,
+                borderColor: 'border.muted',
+                marginTop: '-1px',
+                marginRight: '-1px',
+                borderTopRightRadius: 2,
+                borderBottomLeftRadius: 2,
+              }}
+            />
+            <Box sx={{m: 0, p: 3, overflowX: 'auto'}}>
+              <Box as="pre" className={className} tabIndex={0} sx={{m: 0}}>
+                {tokens.map((line, i) => (
+                  <div key={i} {...getLineProps({line, key: i})}>
+                    {line.map((token, key) => (
+                      <Text key={key} {...getTokenProps({token, key})} sx={{fontFamily: 'mono', fontSize: 1}} />
+                    ))}
+                  </div>
                 ))}
-              </div>
-            ))}
+              </Box>
+            </Box>
           </Box>
-        )}
-      </Highlight>
-    </Box>
+        </Box>
+      )}
+    </Highlight>
   )
 }
 
