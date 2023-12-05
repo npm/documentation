845220afef80b863344cbdc049a893baca188a3b
Luke Karrys <luke@lukekarrys.com>
2023-12-05 10:49:56 -0700

    chore: @npmcli/template-oss@4.21.1

--- cli/scripts/template-oss/index.js
+++ cli/scripts/template-oss/index.js
@@ -2,7 +2,7 @@ module.exports = {
   ...require('../../../scripts/template-oss'),
   workspaceRepo: {
     add: {
-      '.github/workflows/update-cli.yml': 'update-cli.yml',
+      '.github/workflows/update-cli.yml': 'update-cli-yml.hbs',
     },
   },
   allowPaths: ['/releases.json'],
--- scripts/template-oss/index.js
+++ scripts/template-oss/index.js
@@ -1,8 +1,8 @@
 module.exports = {
   rootRepo: {
     add: {
-      '.github/workflows/ci.yml': 'ci.yml',
-      '.github/workflows/publish.yml': 'publish.yml',
+      '.github/workflows/ci.yml': 'ci-yml.hbs',
+      '.github/workflows/publish.yml': 'publish-yml.hbs',
       '.github/CODEOWNERS': false,
       '.github/ISSUE_TEMPLATE/bug.yml': false,
       '.commitlintrc.js': false,
@@ -11,7 +11,7 @@ module.exports = {
   },
   rootModule: {
     add: {
-      'package.json': {file: 'pkg.json', overwrite: false},
+      'package.json': {file: 'package-json.hbs', overwrite: false},
       '.eslintrc.js': false,
       'CODE_OF_CONDUCT.md': false,
       'CONTRIBUTING.md': false,
