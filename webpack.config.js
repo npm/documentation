2c47062e4943dbb900d220beb644d10a60dfa9ac
Gar <gar+gh@danger.computer>
2024-06-12 10:26:30 -0700

    chore: Update webpack.config.js (#1153)
    Add a cautionary comment about `devServer` being a security problem.
    
    ## References
    https://github.com/advisories/GHSA-wr3j-pwj9-hqq6

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const {resolve} = require('path')
+// XXX do not export a devServer from this file.
+// See https://github.com/advisories/GHSA-wr3j-pwj9-hqq6 for more info.
 module.exports = {
   resolve: {
     alias: {
