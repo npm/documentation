c97fd6b3f1c24249c608ae2447c167bdb139a388
Gar <gar+gh@danger.computer>
2024-06-12 09:59:48 -0700

    chore: Update webpack.config.js
    Add a cautionary comment about `devServer` being a security problem.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const {resolve} = require('path')
+// XXX do not export a devServer from this file.
+// See https://github.com/npm/documentation/security/dependabot/112 for more info.
 module.exports = {
   resolve: {
     alias: {
