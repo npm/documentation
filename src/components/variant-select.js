37392ef19dd113ad1bafa5909ad0d620d2454c76
jpg619 <jpg619@github.com>
2025-10-01 15:59:17 -0400

    fix: simplify dependency array in VariantMenu useEffect

--- src/components/variant-select.js
+++ src/components/variant-select.js
@@ -58,7 +58,7 @@ const VariantMenu = ({title, latest, current, prerelease, legacy}) => {
     if (locationChange.change && getNav.didVariantChange(locationChange.previous, locationChange.current)) {
       setOpen(false)
     }
-  }, [locationChange.change, locationChange.current, locationChange.previous])
+  }, [locationChange])
 
   return (
     <>
