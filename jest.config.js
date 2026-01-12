33352528717c7575b86ed995338dfab0af9ab11d
Michael Smith <owlstronaut@github.com>
2026-01-12 11:25:03 -0800

    chore: added transforming patterns to ignore

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,9 @@ module.exports = {
   transform: {
     '^.+\\.jsx?$': `<rootDir>/jest-preprocess.js`,
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!(@primer/react|@primer/octicons-react|@primer/behaviors|@lit-labs|@lit|@github)/)',
+  ],
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
   },
